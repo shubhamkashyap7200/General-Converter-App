caeae5046843e9b4b3502e5820ba51811277e2ad
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-09-30 16:28:14 +0530

    Initial Commit

---
+++ General Converter App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  General Converter App
+//
+//  Created by Shubham on 30/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ General Converter App/General_Converter_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  General_Converter_AppApp.swift
+//  General Converter App
+//
+//  Created by Shubham on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct General_Converter_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
